05d54d9ffd69ee2d82e7246f8954781a51645662
madhyam2001 <madhyampatra12@gmail.com>
2022-09-13 20:32:49 +0530

    Third Commit- Flash Screen

--- app/src/main/java/com/example/in_betweens/MainActivity.java
+++ app/src/main/java/com/example/in_betweens/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.in_betweens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread= new Thread()
+        {
+          @Override
+          public void run()
+          {
+              try
+              {
+                  sleep(7000);
+
+              }
+              catch (Exception e)
+              {
+                  e.printStackTrace();
+
+              }
+              finally
+              {
+                  Intent welcomeIntent = new Intent(MainActivity.this,WelcomeActivity.class);
+                  startActivity(welcomeIntent);
+
+
+              }
+          }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
